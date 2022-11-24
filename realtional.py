e6ba7ab32a335dae18fa6ebfbcf3631cfba287af
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-11-24 22:06:43 +0530

    Add files via upload

---
+++ realtional.py
@@ -0,0 +1,26 @@
+a=int(input('enter a value'))
+b=int(input('enter a value'))
+if(a==b):
+    print('a is equal to b')
+else:
+    print('a is not equal to b')
+if(a!=b):
+    print('a is not equal to b')
+else:
+    print('a is equal to b')
+if(a>b):
+    print('a is greater than b')
+else:
+    print('a is not greater than b')
+if(a<b):
+    print('a is less than b')
+else:
+    print('a is not les than b')
+if(a>=b):
+    print('a is greater than or equal to b')
+else:
+    print('a is not a greater than or not equal to b')
+if(a<=b):
+    print('a is less than or equal to b')
+else:
+    print('a is not less than or not equal to b')
