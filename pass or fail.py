6829d519f3df57bb4682f4bcab9040964d683136
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-11-29 23:08:41 +0530

    Add files via upload

---
+++ pass or fail.py
@@ -0,0 +1,12 @@
+sno=int(input('enter the sno:'))
+sname=input('enter the sname:')
+group=input('enter the group:')
+s1=int(input('enter s1:'))
+s2=int(input('enter s2:'))
+s3=int(input('enter s3:'))
+if s1>=35:
+    if s2>=35:
+        if s3>=35:
+            print('pass')
+else:
+    print('fail')
