c4a4bd6901889bc27bca98a67970791b97ef2785
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-11-29 23:06:16 +0530

    Add files via upload

---
+++ big of four.py
@@ -0,0 +1,20 @@
+a=int(input('enter the value of a:'))
+b=int(input('enter the value of b:'))
+c=int(input('enter the value of c:'))
+d=int(input('enetr the value of d:'))
+if a>b:
+    if a>c:
+        if a>d:
+            print('a is big')
+if b>a:
+    if b>c:
+        if b>d:
+            print('b is big')
+if c>a:
+    if c>b:
+        if c>d:
+            print('c is big')
+if d>a:
+    if d>b:
+        if d>c:
+            print('d is big')
