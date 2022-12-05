5f7dd8a0fe56f6aa4d14a02c44ff75311f95a927
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-12-05 21:07:08 +0530

    Add files via upload

---
+++ prime num btwn 1 to n.py
@@ -0,0 +1,9 @@
+n=int(input('enter n value'))
+for i in range(2,n+1):
+    count=0
+    for j in range(2,i-1):
+        if(i%j==0):
+            count=1
+            break
+        if(count==0):
+            print(i)
