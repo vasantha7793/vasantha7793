3dc00b101ec3fc1d46923cb0bd7ec3342ed507f3
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-12-08 21:50:36 +0530

    Add files via upload

---
+++ matrix series1.py
@@ -0,0 +1,8 @@
+r=int(input('enter the number of rows'))
+c=int(input('enter the number of coloumns'))
+a=9
+for i in range(r):
+    for j in range(c):
+            print(a,end =' ')
+            a=a-1
+            print()
