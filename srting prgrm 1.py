98b998f7d9943b5e894efa0f931e8a19a6e0a781
vasantha7793 <118752094+vasantha7793@users.noreply.github.com>
2022-12-16 21:54:26 +0530

    Add files via upload

---
+++ srting prgrm 1.py
@@ -0,0 +1,18 @@
+str="welcome"
+l=len(str)
+print(l)
+for i in str:
+    print(i)
+
+text = 'WELCOME'
+print(text)
+print(text.lower())
+
+text='welcome'
+print(text)
+print(text.upper())
+
+
+text='weLCOme'
+print(text.lower())
+print(text.upper())
